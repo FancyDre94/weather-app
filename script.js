e7f713875a04460696db04682f37457b1f354912
FancyDre94 <amandaandre61@yahoo.com>
2021-11-20 13:08:20 -0500

    Imported project into GitHub
    Added my .HTML, .JS and .CSS for my weather app into GitHub

---
+++ script.js
@@ -0,0 +1,52 @@
+let now = new Date();
+console.log(now);
+let h2 = document.querySelector("h2");
+
+let days = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday"];
+let day = days[now.getDay()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+h2.innerHTML = `${day} ${hour} : ${minutes}`;
+
+function searchCity(city) {
+  event.preventDefault();
+  let search = document.querySelector("#city-input");
+  let units = "imperial";
+  let apiKey = "e7b34554a2a306cb3853f56f75043cae";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemp);
+}
+
+function displayTemp(response) {
+  console.log(response.data);
+  let cityLocation = document.querySelector("#city");
+  cityLocation.innerHTML = `${response.data.name}`;
+  let temp = document.querySelector("#currentCityTemp");
+  temp.innerHTML = `${Math.round(response.data.main.temp)}℉`;
+}
+
+function showPosition(position) {
+  let units = "imperial";
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "e7b34554a2a306cb3853f56f75043cae";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayTemp);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let current = document.querySelector("#currentLocation");
+current.addEventListener("click", getCurrentPosition);
+let searchResult = document.querySelector("#search-location");
+searchResult.addEventListener("submit", showPosition);
+let searchElement = document.querySelector("form");
+searchElement.addEventListener("submit", searchCity);
